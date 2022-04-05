ba3f6f690b39ad2b2868d799ba2873fb983f63b9
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2022-04-05 11:41:34 +0200

    test

--- Phoneshop.ConsoleApp/Program.cs
+++ Phoneshop.ConsoleApp/Program.cs
@@ -14,7 +14,11 @@ namespace Phoneshop.ConsoleApp
             }
 
             var Phones = new List<string>();
-            Phones.Add(new Phone1 {Brand = "", pType = "", Price = "", Description = "" });
+            Phones.Add(new Phone1 {Brand = "Huawei", pType = "P30", Price = "697", Description = "6.47iinch FHD + (2340x1080) OLED, Kirin 980 Octa - Core(2x Cortex - A76 2.6GHz + 2x Cortex - A76 1.92GHz + 4x Cortex - A55 1.8GHz), 8GB RAM, 128GB ROM, 40 + 20 + 8 + TOF / 32MP, Dual SIM, 4200mAh, Android 9.0 + EMUI 9.1" });
+            Phones.Add(new Phone2 { Brand = "Samsung", pType = "Galaxy A52", Price = "399", Description = "64 megapixel camera, 4k videokwaliteit 6.5 inch AMOLED scherm 128 GB opslaggeheugen (Uitbreidbaar met Micro-sd) Water- en stofbestendig (IP67)" });
+            Phones.Add(new Phone3 { Brand = "Apple", pType = "IPhone 11", Price = "619", Description = "Met de dubbele camera schiet je in elke situatie een perfecte foto of video De krachtige A13-chipset zorgt voor razendsnelle prestaties Met Face ID hoef je enkel en alleen naar je toestel te kijken om te ontgrendelen Het toestel heeft een lange accuduur dankzij een energiezuinige processor" });
+            Phones.Add(new Phone4 { Brand = "Google", pType = "Pixel 4a", Price = "411", Description = "12.2 megapixel camera, 4k videokwaliteit 5.81 inch OLED scherm 128 GB opslaggeheugen 3140 mAh accucapaciteit" });
+            Phones.Add(new Phone5 { Brand = "Xiaomi", pType = "Redmi Note 10 Pro", Price = "298", Description = "108 megapixel camera, 4k videokwaliteit 6.67 inch AMOLED scherm 128 GB opslaggeheugen (Uitbreidbaar met Micro-sd) Water- en stofbestendig (IP53)" });
         }
 
         private static bool MainMenu(string[] args)
