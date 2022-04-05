1384a92eec979041046f387ccac1b06859d61d93
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2022-04-05 11:16:49 +0200

    progress?...

--- Phoneshop.ConsoleApp/Program.cs
+++ Phoneshop.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Phoneshop.ConsoleApp
 {
@@ -6,7 +7,46 @@ namespace Phoneshop.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+
+            var Phones = new List<string>();
+            Phones.Add(new Phone1 {Brand = "", pType = "", Price = "", Description = "" });
+        }
+
+        private static bool MainMenu(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Please choose the desired phone by pressing the corresponding number \n 1. Huawei P30\n 2. Samsung Galaxy A52\n 3. apple IPhone 11 \n 4. Google Pixel 4a \n 5. Xiaomi Redmi Note 10 Pro");
+            var phones = Console.ReadLine();
+
+            switch (Phones)
+            {
+                case "1":
+                    Console.WriteLine(Phone1);
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+
+                case "5":
+
+                    break;
+
+            }
+
         }
     }
 }
