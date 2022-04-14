4e884f4549eb76260a5adb712ff55244f1a57f67
Tamara de Weerd <animegirl_Tayko@hotmail.com>
2022-04-14 11:34:10 +0200

    Working Phoneshop

--- Phoneshop.ConsoleApp/2. Domain/Models/Phone.cs
+++ Phoneshop.ConsoleApp/2. Domain/Models/Phone.cs
@@ -1,14 +1,26 @@
 ﻿using Phoneshop.ConsoleApp._1._Phoneshop.Business;
+using System;
 
 namespace Phoneshop.ConsoleApp
 {
     public class Phone
     {
+        public Phone(string brand, string pType, double priceVAT, string description)
+        {
+            Brand = brand;
+            PType = pType;
+            PriceVAT = priceVAT;
+            Description = description;
+            Id = ++_lastUsedIndex;
+
+        }
+        private static int _lastUsedIndex = 0;
+                           
         public int    Id { get; set; }
         public string Brand { get; set; }
         public string PType { get; set; }
         public double PriceVAT { get; set; }
-        public double Price => PriceVAT * 0.79;
+        public double Price => Math.Round(PriceVAT * 0.79);
 
         public string Description { get; set; }
     }
--- Phoneshop.ConsoleApp/Program.cs
+++ Phoneshop.ConsoleApp/Program.cs
@@ -1,77 +1,85 @@
 ﻿using Phoneshop.ConsoleApp._1._Phoneshop.Business;
 using System;
-using System.Linq;
+
 using System.Text;
 
 namespace Phoneshop.ConsoleApp
 {
     internal class Program
     {
-
-        static public List<Phone> _phones = new();
-
-
+        static readonly PhoneService phoneService = new();
         static void Main()
         {
             bool showMenu = true;
 
-            //add phones to list
-            _phones.Add(new Phone { Brand = "Huawei", pType = "P30", Price = "697", Description = "6.47inch FHD + (2340x1080) OLED, Kirin 980 Octa - Core(2x Cortex - A76 2.6GHz + 2x Cortex - A76 1.92GHz + 4x Cortex - A55 1.8GHz), 8GB RAM, 128GB ROM, 40 + 20 + 8 + TOF / 32MP, Dual SIM, 4200mAh, Android 9.0 + EMUI 9.1" });
-            _phones.Add(new Phone { Brand = "Samsung", pType = "Galaxy A52", Price = "399", Description = "64 megapixel camera, 4k videokwaliteit 6.5 inch AMOLED scherm 128 GB opslaggeheugen (Uitbreidbaar met Micro-sd) Water- en stofbestendig (IP67)" });
-            _phones.Add(new Phone { Brand = "Apple", pType = "IPhone 11", Price = "619", Description = "Met de dubbele camera schiet je in elke situatie een perfecte foto of video De krachtige A13-chipset zorgt voor razendsnelle prestaties Met Face ID hoef je enkel en alleen naar je toestel te kijken om te ontgrendelen Het toestel heeft een lange accuduur dankzij een energiezuinige processor" });
-            _phones.Add(new Phone { Brand = "Google", pType = "Pixel 4a", Price = "411", Description = "12.2 megapixel camera, 4k videokwaliteit 5.81 inch OLED scherm 128 GB opslaggeheugen 3140 mAh accucapaciteit" });
-            _phones.Add(new Phone { Brand = "Xiaomi", pType = "Redmi Note 10 Pro", Price = "298", Description = "108 megapixel camera, 4k videokwaliteit 6.67 inch AMOLED scherm 128 GB opslaggeheugen (Uitbreidbaar met Micro-sd) Water- en stofbestendig (IP53)" });
-            
             while (showMenu)
             {
                 showMenu = MainMenu();
                 Console.ReadLine();
             }
-
-
         }
 
         public static bool MainMenu()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            
+            var _phones = phoneService._phones;
             //TODO zorgen voor iets wat verkeerde input opvangt
             Console.Clear();
-            var phoneService = new PhoneService();
-            var _phones = phoneService._phones;
+
             Console.WriteLine("Please choose the desired phone by pressing the corresponding number\n");
             foreach (Phone phone in _phones)
             {
-                Console.WriteLine($"{_phones.Count} {_phones[0].Brand} \t{_phones[0].PType}");
+                Console.WriteLine($"{phone.Id} {phone.Brand} \t{phone.PType}");
             }
-            
-             //Link to List in PhoneService, getphone?
-           
+
+            //Link to List in PhoneService, getphone?
+
 
             var userInput = Console.ReadKey(); //TODO kijken of did niet beter kan
-            int phones;
+            int phoneId;
 
             if (char.IsDigit(userInput.KeyChar))
             {
-                phones = int.Parse(userInput.KeyChar.ToString());
-                Console.Clear();
-                Console.WriteLine($"{_phones[0].Brand} {_phones[0].PType} €{_phones[0].PriceVAT} without VAT € - \n{_phones[0].Description}");
-                Console.WriteLine("Press any to go back to the main menu\n");
-                Console.ReadKey();
-                return MainMenu();
+                phoneId = int.Parse(userInput.KeyChar.ToString());
+                var chosenPhone = _phones.Find(x => x.Id == phoneId);
+                if (chosenPhone != null)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{chosenPhone.Brand} {chosenPhone.PType} €{chosenPhone.PriceVAT} without VAT €{chosenPhone.Price} \n{chosenPhone.Description}");
+                    Console.WriteLine("Press any to go back to the main menu\n");
+                    Console.ReadKey();
+                    return MainMenu();
+                }
+
+                else
+                {
+
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("User did not chose one of the available numbers, press any to go back to the main menu\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                    return MainMenu();
+
+                }
+
+
+
+
             }
 
             else
             {
-                phones = -1;
+                Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("User did not input a number");
+                Console.WriteLine("User did not input a number press any key to return to main menu.\n");
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.ReadKey();
                 return MainMenu();
             }
-            
-            return true;
+
+
         }
     }
 }
+
